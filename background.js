0cbabe0b97156d871680753bd0237bb225a66c34
Stefan <stefan.armijo@gmail.com>
2018-05-20 15:09:35 -0700

    fix background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.runtime.onInstalled.addListenter(function() {
+chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log('the color is whatever');
   });
